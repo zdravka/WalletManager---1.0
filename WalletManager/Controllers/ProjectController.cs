64057dfb77e3fc167645f47f45562f2f42dc95e5
zdrav4enceto"gmail.com <zdravka12>
2014-06-07 18:10:09 +0300

    design

---
+++ WalletManager/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WalletManager.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Uni
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
